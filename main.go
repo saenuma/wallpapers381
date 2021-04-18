cd252e8c9e57075191c6895910497e1f3584379e
Bankole Ojo <ojobankole@gmail.com>
2021-04-18 12:43:13 +0100

    added testing of text files

--- main.go
+++ main.go
@@ -33,25 +33,9 @@ func main() {
   if err != nil {
     panic(err)
   }
-  hex, err := colors.ParseHEX("#C9B466")
-  nCR := hex.ToRGBA()
-  newColor := color.RGBA{uint8(nCR.R), uint8(nCR.G), uint8(nCR.B), uint8(nCR.A)}
-  bg, fg := image.Black, image.NewUniform(newColor)
-
 
   screenBounds := screenshot.GetDisplayBounds(0)
-
   rgba := image.NewRGBA(image.Rect(0, 0, screenBounds.Dx(), screenBounds.Dy()))
-	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
-	c := freetype.NewContext()
-	c.SetDPI(DPI)
-	c.SetFont(fontParsed)
-	c.SetFontSize(SIZE)
-	c.SetClip(rgba.Bounds())
-	c.SetDst(rgba)
-	c.SetSrc(fg)
-  c.SetHinting(font.HintingNone)
-
   fontDrawer := &font.Drawer{
     Dst: rgba,
     Src: image.Black,
@@ -62,37 +46,69 @@ func main() {
     }),
   }
 
-  textBytes, err := embeddedTexts.ReadFile(randTextFile())
-  if err != nil {
-    panic(err)
+  if len(os.Args) == 2 && os.Args[1] == "t" {
+    dirFIs, err := embeddedTexts.ReadDir("texts")
+    if err != nil {
+      panic(err)
+    }
+    for _, dirFI := range dirFIs {
+      f := filepath.Join("texts", dirFI.Name())
+      textBytes, _ := embeddedTexts.ReadFile(f)
+      texts := wordWrap(string(textBytes), screenBounds.Dx() - 200, fontDrawer)
+      if len(texts) > 5 {
+        panic(fmt.Sprintf("%s is more than five lines after word wrapping. Please make shorter.", f))
+      }
+    }
+  } else {
+    textBytes, err := embeddedTexts.ReadFile(randTextFile())
+    if err != nil {
+      panic(err)
+    }
+    texts := wordWrap(string(textBytes), screenBounds.Dx() - 200, fontDrawer)
+
+    hex, err := colors.ParseHEX("#C9B466")
+    nCR := hex.ToRGBA()
+    newColor := color.RGBA{uint8(nCR.R), uint8(nCR.G), uint8(nCR.B), uint8(nCR.A)}
+    bg, fg := image.Black, image.NewUniform(newColor)
+
+
+  	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
+  	c := freetype.NewContext()
+  	c.SetDPI(DPI)
+  	c.SetFont(fontParsed)
+  	c.SetFontSize(SIZE)
+  	c.SetClip(rgba.Bounds())
+  	c.SetDst(rgba)
+  	c.SetSrc(fg)
+    c.SetHinting(font.HintingNone)
+
+    // Draw the text.
+  	pt := freetype.Pt(100, 100+int(c.PointToFixed(SIZE)>>6))
+  	for _, s := range texts {
+  		_, err = c.DrawString(s, pt)
+  		if err != nil {
+  			panic(err)
+  		}
+  		pt.Y += c.PointToFixed(SIZE * SPACING)
+  	}
+
+    // Save that RGBA image to disk.
+  	outFile, err := os.Create("/tmp/out.png")
+  	if err != nil {
+      panic(err)
+  	}
+  	defer outFile.Close()
+  	b := bufio.NewWriter(outFile)
+  	err = png.Encode(b, rgba)
+  	if err != nil {
+      panic(err)
+  	}
+  	err = b.Flush()
+  	if err != nil {
+      panic(err)
+  	}
   }
-  texts := wordWrap(string(textBytes), screenBounds.Dx() - 200, fontDrawer)
 
-  // Draw the text.
-	pt := freetype.Pt(100, 100+int(c.PointToFixed(SIZE)>>6))
-	for _, s := range texts {
-		_, err = c.DrawString(s, pt)
-		if err != nil {
-			panic(err)
-		}
-		pt.Y += c.PointToFixed(SIZE * SPACING)
-	}
-
-  // Save that RGBA image to disk.
-	outFile, err := os.Create("/tmp/out.png")
-	if err != nil {
-    panic(err)
-	}
-	defer outFile.Close()
-	b := bufio.NewWriter(outFile)
-	err = png.Encode(b, rgba)
-	if err != nil {
-    panic(err)
-	}
-	err = b.Flush()
-	if err != nil {
-    panic(err)
-	}
 }
 
 
@@ -101,14 +117,14 @@ func randTextFile() string {
   if err != nil {
     panic(err)
   }
-  fonts := make([]string, 0)
+  texts := make([]string, 0)
   for _, dirFI := range dirFIs {
     f := filepath.Join("texts", dirFI.Name())
-    fonts = append(fonts, f)
+    texts = append(texts, f)
   }
 
   var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
-  return fonts[seededRand.Intn(len(fonts))]
+  return texts[seededRand.Intn(len(texts))]
 }
 
 
